4813f55e386d5c3ded604337384742f129479d2b
obarbozab <obarboza@twilio.com>
2024-08-01 23:11:20 -0600

    new script for WA

--- Whatsapp/quick_reply_template.py
+++ Whatsapp/quick_reply_template.py
@@ -1,6 +1,5 @@
 import os
 from twilio.rest import Client
-import time
 from dotenv import load_dotenv
 import json
 
@@ -12,37 +11,22 @@ auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 client = Client(account_sid,auth_token)
 
 twilio_response = client.messages.create(
-  #from_='+18017972087',
-  from_= '+12058833479',
-  body =f'Test message',
-  #to ='+16168284433'
-  to = os.environ.get('my_number')
-  #to = '+12542218604'
+    content_sid="HX2af507311fd99fbb57b3b93383ff6d6f",
+    to= os.environ.get("my_wa_number"),
+    from_='MGed2ab7fa9ab8d197b19404aad1b7524f',
+        content_variables=json.dumps({
+            '1': "Johnny Pardo",
+            '2': "DragonCEM",
+            '3': "Smoky Mangoose",
+            '4': "Google Ads"
+            }),
 )
 
-
-# Wait for a few seconds to allow the status to update
-#time.sleep(5)  # Delays for 5 seconds. You can change the number to any amount of seconds you need.
-
-# Fetch the message again to get the updated status
-#message_updated = client.messages.get(twilio_response.sid).fetch()
-
-# Print the final status, error code, and error message
-
-'''print(f"Message SID: {message_updated.sid} | Final Status: {message_updated.status}")
-if message_updated.error_code:
-    print(f"Error Code: {message_updated.error_code}")
-    print(f"Error Message: {message_updated.error_message}")
-else:
-    print("Message sent successfully")
-
-'''
 # load api response 
 api_response = client.http_client._test_only_last_response
 
 # Parse the JSON response content
 response_data = json.loads(api_response.content)
-
 
 print("---------------------------------------------------------------------------------------")
 print(f" Message SID: {twilio_response.sid} | Error code: {twilio_response.error_code} | Error message: {twilio_response.error_message}")
